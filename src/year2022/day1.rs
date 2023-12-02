5acd61c750fe4a76407efead49d3a6ecb794278b
Hector Peeters <hector.peeters@gmail.com>
2023-12-02 12:13:07 +0100

    Opimize 2022 day 1

--- src/year2022/day1.rs
+++ src/year2022/day1.rs
@@ -12,31 +12,21 @@ impl Solution for Solution2022Day1 {
     fn parse(&mut self) -> Result<()> {
         self.data = read_input_file("src/year2022/day1.txt")?
             .split("\n\n")
-            .map(|x| {
-                x.split('\n')
-                    .filter(|x| !x.is_empty())
-                    .map(|y| y.parse().unwrap())
-                    .collect()
-            })
+            .map(|x| x.split('\n').filter_map(|y| y.parse().ok()).collect())
             .collect::<Vec<Vec<u64>>>();
 
         Ok(())
     }
 
     fn part1(&self) -> Result<u64> {
-        Ok(self
-            .data
-            .iter()
-            .map(|x| x.iter().copied().sum())
-            .max()
-            .unwrap())
+        Ok(self.data.iter().map(|x| x.iter().sum()).max().unwrap())
     }
 
     fn part2(&self) -> Result<u64> {
         Ok(self
             .data
             .iter()
-            .map(|x| x.iter().copied().sum::<u64>())
+            .map(|x| x.iter().sum::<u64>())
             .sorted_unstable()
             .rev()
             .take(3)
